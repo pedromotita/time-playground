f89481076218030fe617db8b5a7e1e0ff68dda02
pmotita <p.smota98@gmail.com>
2022-04-08 18:21:24 -0300

    Encapsulated timer logic in TimeProvider class

--- Time.swiftpm/ContentView.swift
+++ Time.swiftpm/ContentView.swift
@@ -3,45 +3,24 @@ import Combine
 
 struct ContentView: View {
     
-    @State var remainingTime = 15
-    @State var timerSubscription: Cancellable?
-    
-    @State var timer = Timer.TimerPublisher(interval: 1.0, runLoop: .main, mode: .common)
-    
-    private func instantiateTimer() {
-        self.timer = Timer.TimerPublisher(interval: 1.0, runLoop: .main, mode: .common)
-    }
-    
-    private func startTimer() {
-        self.remainingTime = 15
-        self.instantiateTimer()
-        self.timerSubscription = timer.connect()
-    }
-    
-    private func cancelTimer() {
-        self.timerSubscription?.cancel()
-    }
+    @StateObject private var timer = TimerProvider()
     
     var body: some View {
         VStack(alignment: .center) {
             Spacer()
-            Text("\(remainingTime)")
+            Text("\(timer.remainingTime)")
+                .onReceive(timer.timerPublisher) { _ in
+                    timer.uptadeRemainingTime()
+                }
                 .foregroundColor(Color.white)
                 .frame(maxWidth: .infinity)
                 .font(.title)
             Spacer()
             LargeButton {
-                self.startTimer()
+                timer.startTimer()
             }
                 .padding(.horizontal, 32)
                 .padding(.bottom, 40)
-        }
-        .onReceive(timer) { _ in
-            if remainingTime <= 1 {
-                self.cancelTimer()
-            } else {
-                remainingTime = remainingTime - 1
-            }
         }
         .frame(maxHeight: .infinity)
         .background(Color.init("dark-blue"))
