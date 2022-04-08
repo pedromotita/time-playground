1e2cb7edf6e57af28604dd0dce39c07356e75f84
Pedro Mota <pedromota@Pedros-MacBook-Pro.local>
2022-04-08 17:39:50 -0300

    Timer logic and LargeButton refactor

--- Time.swiftpm/ContentView.swift
+++ Time.swiftpm/ContentView.swift
@@ -1,8 +1,26 @@
 import SwiftUI
+import Combine
 
 struct ContentView: View {
     
-    @State private var remainingTime = 15
+    @State var remainingTime = 15
+    @State var timerSubscription: Cancellable?
+    
+    @State var timer = Timer.TimerPublisher(interval: 1.0, runLoop: .main, mode: .common)
+    
+    private func instantiateTimer() {
+        self.timer = Timer.TimerPublisher(interval: 1.0, runLoop: .main, mode: .common)
+    }
+    
+    private func startTimer() {
+        self.remainingTime = 15
+        self.instantiateTimer()
+        self.timerSubscription = timer.connect()
+    }
+    
+    private func cancelTimer() {
+        self.timerSubscription?.cancel()
+    }
     
     var body: some View {
         VStack(alignment: .center) {
@@ -12,9 +30,18 @@ struct ContentView: View {
                 .frame(maxWidth: .infinity)
                 .font(.title)
             Spacer()
-            LargeButton()
+            LargeButton {
+                self.startTimer()
+            }
                 .padding(.horizontal, 32)
                 .padding(.bottom, 40)
+        }
+        .onReceive(timer) { _ in
+            if remainingTime <= 1 {
+                self.cancelTimer()
+            } else {
+                remainingTime = remainingTime - 1
+            }
         }
         .frame(maxHeight: .infinity)
         .background(Color.init("dark-blue"))
